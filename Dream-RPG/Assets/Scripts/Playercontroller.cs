f7f9a487e30adb7ddd1c83fb6a3a73cca5125c6c
Rajudev <rajussp31121997@gmail.com>
2022-02-16 20:41:13 +0530

    attch arrow on runtime fixed and rotation along with bow bug fixed

--- Dream-RPG/Assets/Scripts/Playercontroller.cs
+++ Dream-RPG/Assets/Scripts/Playercontroller.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:b79175d0b56ab5933ef530966f7075f70d24bb702a64cf8c9b67e4a9146b781e
-size 7551
+oid sha256:af674b986bc5371daa1e19d741ba90df29059e12bc61c058626ecafec8a53d5a
+size 7536
